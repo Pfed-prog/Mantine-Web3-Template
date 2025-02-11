8eed5435647209fa6e7ce29656374b76df778d25
name <fedorovn19@gmail.com>
2025-02-11 17:38:16 +0300

    fixing eslint

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,5 +1,5 @@
-import { ActionIcon, Group, useMantineColorScheme } from "@mantine/core";
-import { IconSun, IconMoonStars } from "@tabler/icons-react";
+import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
 export function ColorSchemeToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -11,16 +11,11 @@ export function ColorSchemeToggle() {
         size="xl"
         sx={(theme) => ({
           backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-          color:
-            theme.colorScheme === "dark"
-              ? theme.colors.yellow[4]
-              : theme.colors.blue[6],
+            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+          color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
         })}
       >
-        {colorScheme === "dark" ? (
+        {colorScheme === 'dark' ? (
           <IconSun size={20} stroke={1.5} />
         ) : (
           <IconMoonStars size={20} stroke={1.5} />
